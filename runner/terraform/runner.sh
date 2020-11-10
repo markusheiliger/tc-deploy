9b68cb206e6faaca2921d10652b4c8407c1e54c1
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-10 14:02:01 -0800

    ...

--- runner/terraform/runner.sh
+++ runner/terraform/runner.sh
@@ -4,28 +4,26 @@ trace() {
     echo ">>> $@ ..."
 }
 
+trace "Selecting template directory"
+cd $(echo "$(dirname $EnvironmentTemplate)" | sed 's/^file:\/\///') && echo $PWD
 
 trace "Connecting Azure"
 while true; do
     # managed identity isn't avaialble directly - retry
-    az login --identity 2>/dev/null && {
+    az login --identity --output none 2>/dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
         export ARM_SUBSCRIPTION_ID=$EnvironmentSubscription
+        az account set --subscription $EnvironmentSubscription
+        az account show
         break
     } || sleep 5    
 done
 
-trace "Selecting subscription"
-az account set --subscription $EnvironmentSubscription
-
-trace "Selecting template"
-cd $(echo "$(dirname $EnvironmentTemplate)" | sed 's/^file:\/\///') && echo $PWD
-
-trace "Initializing terraform"
+trace "Initializing Terraform"
 terraform init
 
-trace "Applying terraform"
+trace "Applying Terraform Plan"
 terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroup"
 
 tail -f /dev/null
