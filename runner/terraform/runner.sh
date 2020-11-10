aba113cd30834a3f409cf2696c122bb90736f1f1
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-09 17:50:55 -0800

    ...

--- runner/terraform/runner.sh
+++ runner/terraform/runner.sh
@@ -11,18 +11,18 @@ while true; do
     az login --identity 2>/dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
-        export ARM_SUBSCRIPTION_ID=$EnvironmentResourceGroupSubscription
+        export ARM_SUBSCRIPTION_ID=$EnvironmentSubscription
         break
     } || sleep 5    
 done
 
 trace "Selecting subscription"
-az account set --subscription $EnvironmentResourceGroupSubscription
+az account set --subscription $EnvironmentSubscription
 
 trace "Initializing terraform"
-echo "$EnvironmentTemplate" |  sed 's/file:\/\///' | cd && terraform init
+echo "$(dirname $EnvironmentTemplate)" |  sed 's/file:\/\///' | cd && terraform init
 
 trace "Applying terraform"
-terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
+terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroup"
 
 tail -f /dev/null
