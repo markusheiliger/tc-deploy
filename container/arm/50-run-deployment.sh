36b7983ed5b90c2abfe927d813ed173522a7360b
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-10 18:17:21 -0800

    ...

--- container/arm/50-run-deployment.sh
+++ container/arm/50-run-deployment.sh
@@ -4,22 +4,19 @@ trace() {
     echo ">>> $@ ..."
 }
 
-trace "Selecting template directory"
-cd $(echo "$(dirname $EnvironmentTemplate)" | sed 's/^file:\/\///') && echo $PWD
 
-trace "Connecting Azure"
 while true; do
     # managed identity isn't avaialble directly - retry
     az login --identity --output none 2>/dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
         export ARM_SUBSCRIPTION_ID=$EnvironmentSubscription
-        az account set --subscription $EnvironmentSubscription
-        az account show
         break
     } || sleep 5    
 done
 
+trace "Selecting target subscription"
+az account set --subscription $EnvironmentSubscription && az account show
 
-
-tail -f /dev/null
+trace "Selecting template directory"
+cd $(echo "$(dirname $EnvironmentTemplate)" | sed 's/^file:\/\///') && echo $PWD
