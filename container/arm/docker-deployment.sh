fb8b121e092a7b28d9f492a7f858cb9bd4867f9a
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-12 11:05:27 -0800

    ...

--- container/arm/docker-deployment.sh
+++ container/arm/docker-deployment.sh
@@ -1,20 +1,47 @@
 #!/bin/sh
 
+DeploymentName="$(uuidgen)"
+
+track() { 
+    
+    echo "$1"; 
+}
+
 if [ -z "$EnvironmentResourceGroup" ]; then
 
     az deployment sub create    --location "$EnvironmentLocation" \
-                                --name "$DeploymentId" \
-                                --no-prompt true --verbose \
+                                --name "$DeploymentName" \
+                                --no-prompt true --no-wait \
                                 --template-uri "$EnvironmentTemplateUrl" 
 
 else
 
     az deployment group create  --resource-group "$EnvironmentResourceGroup" \
-                                --name "$DeploymentId" \
-                                --no-prompt true --verbose \
+                                --name "$DeploymentName" \
+                                --no-prompt true --no-wait \
                                 --template-uri "$EnvironmentTemplateUrl" 
 
+    while true; do
+
+        sleep 5
+
+        $ProvisioningState=$(az deployment group show --resource-group "$EnvironmentResourceGroup" --name "$DeploymentName" --query "properties.provisioningState" -o tsv)
+
+        case "${ProvisioningState^^}" in
+            *)
+                track $(az deployment operation group list --resource-group "$EnvironmentResourceGroup" --name "$DeploymentName")
+                ;;&
+            "CANCELED" | "FAILED" | "SUCCEEDED")
+                echo "DEPLOYMENT ${ProvisioningState^^}"
+                break
+        esac
+
+    done
 fi
+
+
+
+
 
 tail -f /dev/null
 
