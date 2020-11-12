d43a2690e396622b9bd9d964434a548bda47cbb3
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-11 20:56:32 -0800

    ...

--- container/_core/runner/docker-entrypoint.sh
+++ container/_core/runner/docker-entrypoint.sh
@@ -21,20 +21,11 @@ fi
 if [[ ! -z "$EnvironmentSubscription" ]]; then
 
     trace "Connecting Azure"
-    while true; do
-        
-        # managed identity isn't avaialble directly - retry
-        az login --identity --output none 2>/dev/null && {
-            export ARM_USE_MSI=true
-            export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
-            export ARM_SUBSCRIPTION_ID=$EnvironmentSubscription
-            break
-        } || { 
-            echo "retry in 5 sec ..." 
-            sleep 5 
-        }      
-
-    done
+    az login --identity && {
+        export ARM_USE_MSI=true
+        export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
+        export ARM_SUBSCRIPTION_ID=$EnvironmentSubscription
+    }
 
     az account set --subscription $EnvironmentSubscription && \
     az account show
