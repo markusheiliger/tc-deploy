dcc10bc910fc72fd35e9cb354f2e2cef201afcae
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-12 16:10:53 -0800

    ...

--- container/_core/runner/docker-entrypoint.sh
+++ container/_core/runner/docker-entrypoint.sh
@@ -9,7 +9,7 @@ trace() {
 if [[ ! -z "$DeploymentHost" ]]; then
     nginx
     sed -i "s/server_name.*/server_name $DeploymentHost;/g" /etc/nginx/conf.d/default.conf
-    certbot --nginx --register-unsafely-without-email --agree-tos -n -d $DeploymentHost
+    certbot --nginx --register-unsafely-without-email --agree-tos --quiet -n -d $DeploymentHost
 fi
 
 find "/docker-entrypoint.d/" -follow -type f -iname "*.sh" -print | sort -n | while read -r f; do
