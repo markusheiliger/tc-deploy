23f0433c4f0a097e1957e9846c2a27fa140ae459
Markus Heiliger <markus.heiliger@microsoft.com>
2020-11-13 16:54:28 -0800

    ...

--- container/arm/docker-deployment.sh
+++ container/arm/docker-deployment.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-DeploymentName="$(uuidgen)"
-
-track() { 
+trackDeployment() { 
     
     echo "$1"; 
 }
 
+EnvironmentTemplateUrlSecure="$(echo "$EnvironmentTemplateUrl" | sed 's/^http:/https:/g')"
+EnvironmentDeploymentName="$(uuidgen)"
+EnvironmentTemplateParametersJson=$(echo "$EnvironmentTemplateParameters" | jq --compact-output '{ "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#", "contentVersion": "1.0.0.0", "parameters": (to_entries | if length == 0 then {} else (map( { (.key): { "value": .value } } ) | add) end) }' )
+EnvironmentTemplateParametersOpts=()
+
+$(cat "$EnvironmentTemplateFile" | jq --raw-output '.parameters | to_entries[] | select( .key | startswith("_artifactsLocation")) | .key' ) | while read p; do
+    case "$p"
+        "_artifactsLocation")
+            EnvironmentTemplateParametersOpts+=( --parameters _artifactsLocation="$(dirname EnvironmentTemplateUrlSecure)" )
+            ;;
+        "_artifactsLocationSasToken")
+            EnvironmentTemplateParametersOpts+=( --parameters _artifactsLocation="?code=EnvironmentTemplateUrlToken" )
+            ;;
+    esac
+done
+
 if [ -z "$EnvironmentResourceGroup" ]; then
 
     az deployment sub create    --location "$EnvironmentLocation" \
-                                --name "$DeploymentName" \
+                                --name "$EnvironmentDeploymentName" \
                                 --no-prompt true --no-wait \
-                                --template-uri "$EnvironmentTemplateUrl" 
-
-else
-
-    az deployment group create  --resource-group "$EnvironmentResourceGroup" \
-                                --name "$DeploymentName" \
-                                --no-prompt true --no-wait \
-                                --template-uri "$EnvironmentTemplateUrl"
+                                --template-uri "$EnvironmentTemplateUrlSecure" \
+                                --parameters "$EnvironmentTemplateParametersJson" \
+                                "${EnvironmentTemplateParametersOpts[@]}"
 
     if [ $? -eq 0 ]; then # deployment successfully created
 
@@ -27,14 +36,42 @@ else
 
             sleep 5
 
-            ProvisioningState=$(az deployment group show --resource-group "$EnvironmentResourceGroup" --name "$DeploymentName" --query "properties.provisioningState" -o tsv)
-            ProvisioningDetails=$(az deployment operation group list --resource-group "$EnvironmentResourceGroup" --name "$DeploymentName")
+            ProvisioningState=$(az deployment sub show --name "$EnvironmentDeploymentName" --query "properties.provisioningState" -o tsv)
+            ProvisioningDetails=$(az deployment operation sub list --name "$EnvironmentDeploymentName")
+
+            trackDeployment "$ProvisioningDetails"
+            
+            if [[ "CANCELED|FAILED|SUCCEEDED" == *"${ProvisioningState^^}"* ]]; then
+
+                    echo "Deployment $EnvironmentDeploymentName: $ProvisioningState"
+                    break
+            fi
+
+        done
+    fi
+
+else
+
+    az deployment group create  --resource-group "$EnvironmentResourceGroup" \
+                                --name "$EnvironmentDeploymentName" \
+                                --no-prompt true --no-wait \
+                                --template-uri "$EnvironmentTemplateUrlSecure" \
+                                --parameters "$EnvironmentTemplateParametersJson"
+
+    if [ $? -eq 0 ]; then # deployment successfully created
+
+        while true; do
+
+            sleep 5
+
+            ProvisioningState=$(az deployment group show --resource-group "$EnvironmentResourceGroup" --name "$EnvironmentDeploymentName" --query "properties.provisioningState" -o tsv)
+            ProvisioningDetails=$(az deployment operation group list --resource-group "$EnvironmentResourceGroup" --name "$EnvironmentDeploymentName")
 
             track "$ProvisioningDetails"
             
             if [[ "CANCELED|FAILED|SUCCEEDED" == *"${ProvisioningState^^}"* ]]; then
 
-                    echo "Deployment $DeploymentName: $ProvisioningState"
+                    echo "Deployment $EnvironmentDeploymentName: $ProvisioningState"
                     break
             fi
 
